8611c55c6a1e8c2e06707e69db12d13c21697bb5
bendamian <badamian@gmail.com>
2026-03-30 20:06:42 +0100

    add live search

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search_contacts, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Contact
+from django.db.models import Q
 
 # Create your views here.
 
@@ -11,3 +12,15 @@ def index(request):
         'contacts': contacts
     }
     return render(request, 'index.html', context)
+
+
+def search_contacts(request):
+    query = request.GET.get('search', '')
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    ).order_by('-created_at') if request.user.is_authenticated else []
+
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'partials/contact-list.html', context)
